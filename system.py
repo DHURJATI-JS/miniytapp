7228c833bf44b966f18e022e403f264349f54e19
Dhurjati <77923926+DHURJATI-JS@users.noreply.github.com>
2026-04-12 18:45:46 +0530

    Update system.py

--- system.py
+++ system.py
@@ -173,8 +173,8 @@ mostsearched=["Search for how to make a cake...",
     "Search for how to use minicpm...",
     "Search for coordinate geometry..."]
 category=[x.lower() for x in category ]
-myemail="dhurjatisharma2010@gmail.com"
-myemailkey="nifn azve dngv sibi"
+myemail=""
+myemailkey=""
 def get_video_duration(file_path):
     clip = VideoFileClip(file_path)
     duration = clip.duration  
